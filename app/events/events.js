8e8e653a5e987a1b4f096b426e103183f382590f
Chule79 <jsantoslopez99@gmail.com>
2022-10-12 20:00:02 +0200

    ultimo

--- app/events/events.js
+++ app/events/events.js
@@ -9,38 +9,38 @@ import { getSelectCharacter } from "../pages/selectCharacter";
 
 
 export const addEvents = () => {
-    eventSearch()
-    eventIndex()
-    eventSAS()
-    eventWarcom()
-    eventChimera()
-    eventCia()
-    eventMilicia()
-    eventArmisticio()
-    eventQatala()
-    eventSpetsnaz()  
+    eventSearch();
+    eventIndex();
+    eventSAS();
+    eventWarcom();
+    eventChimera();
+    eventCia();
+    eventMilicia();
+    eventArmisticio();
+    eventQatala();
+    eventSpetsnaz();  
   }
   
 
 
 
 
-export const eventInicio =  (e) => {
+export const eventInicio =  () => {
  const iniciar = document.querySelector("#inicioBtn");
-    iniciar.addEventListener("click", () => getCharacters())
-    ;
+    iniciar.addEventListener("click", () => getCharacters());
+    
 }
 
 
 
-export const eventSearch =  (a) =>{
+export const eventSearch =  () =>{
     const search = document.querySelector("#btSearch");
-    search.addEventListener("click", (e) => {
-        const searchInput = document.querySelector("input[type=search]").value
-        const nameSearch = searchInput.charAt(0).toUpperCase() + searchInput.substring(1)
-        let valorBusca = nameSearch
-        const data = dataList
-        searchCharacter(data, valorBusca)
+    search.addEventListener("click", () => {
+        const searchInput = document.querySelector("input[type=search]").value;
+        const nameSearch = searchInput.charAt(0).toUpperCase() + searchInput.substring(1);
+        let valorBusca = nameSearch;
+        const data = dataList;
+        searchCharacter(data, valorBusca);
     });
     
 }
@@ -48,47 +48,47 @@ export const eventSearch =  (a) =>{
 
 
 
-export const eventIndex =  (e) => {
+export const eventIndex =  () => {
     const index = document.querySelector("#inicio");
         index.addEventListener("click", () => getIndex());
 }
 
-export const eventSAS =  (e) => {
+export const eventSAS =  () => {
     const Sas = document.querySelector("#SAS");
         Sas.addEventListener("click", () => getSas());
 }
 
-export const eventWarcom =  (e) => {
+export const eventWarcom =  () => {
     const Warcom = document.querySelector("#Warcom");
         Warcom.addEventListener("click", () => getWarcom());
 }
 
-export const eventChimera =  (e) => {
+export const eventChimera =  () => {
     const Chimera = document.querySelector("#Chimera");
         Chimera.addEventListener("click", () => getChimera());
 }
 
-export const eventCia=  (e) => {
+export const eventCia=  () => {
     const Cia = document.querySelector("#CIA");
        Cia.addEventListener("click", () => getCia());
 }
 
-export const eventMilicia=  (e) => {
+export const eventMilicia=  () => {
     const Milicia = document.querySelector("#Milicia");
         Milicia.addEventListener("click", () => getMilicia());
 }
 
-export const eventArmisticio =  (e) => {
+export const eventArmisticio =  () => {
     const Armisticio = document.querySelector("#Armisticio");
         Armisticio.addEventListener("click", () => getArmisticio());
 }
 
-export const eventQatala =  (e) => {
+export const eventQatala =  () => {
     const Qatala = document.querySelector("#Qatala");
         Qatala.addEventListener("click", () => getQatala());
 }
 
-export const eventSpetsnaz =  (e) => {
+export const eventSpetsnaz =  () => {
     const Spetsnaz =  document.querySelector("#Spetsnaz");
         Spetsnaz.addEventListener("click", () => getSpetsnaz());
 }
@@ -104,10 +104,10 @@ export const eventCharacter =  () =>{
     select.forEach((sele) => {
         console.log(sele);
         sele.addEventListener("click", (eleg) => {
-            const pls = eleg.target.dataset.idOper
+            const pls = eleg.target.dataset.idOper;
             console.log(pls);
-            const data = dataList
-            getSelectCharacter(data, pls)
+            const data = dataList;
+            getSelectCharacter(data, pls);
         })
       })
 }
