5cbf702050d97df0cbcdb0ff5e38521655bebe51
Chule79 <jsantoslopez99@gmail.com>
2022-10-12 18:50:03 +0200

    Error: Select personaje

--- app/service/api.js
+++ app/service/api.js
@@ -1,7 +1,7 @@
 
 export const getData = async () => {
     try {
-        const conexionData = await fetch("http://localhost:8080/Characters")
+        const conexionData = await fetch("https://cod-intelligence.onrender.com/Characters")
         const translateData = await conexionData.json();
         return translateData
     } catch (error) {
